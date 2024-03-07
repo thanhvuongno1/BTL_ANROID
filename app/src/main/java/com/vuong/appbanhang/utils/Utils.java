d0dff36d0a9cb86c90b48e87da752ad2ef502529
Nguyễn Vượng <Asus@ThanhVuong.VN>
2024-03-07 15:45:05 +0700

    thuan

--- app/src/main/java/com/vuong/appbanhang/utils/Utils.java
+++ app/src/main/java/com/vuong/appbanhang/utils/Utils.java
@@ -1,6 +1,6 @@
 package com.vuong.appbanhang.utils;
 
 public class Utils {
-    public static final String BASE_URL="http://192.168.1.86/banhang/";
+    public static final String BASE_URL="http://172.20.10.2/banhang/";
 
 }
